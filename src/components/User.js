d881e325f23e0315651d0a08fc5968c9b5b2c194
Tej Patel <tejpatel1806@gmail.com>
2024-01-02 11:10:37 +0530

    addinguserpage

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+const User = () => {
+    return (
+      <>
+        <div className="user-card">
+          <h2>Name:Tej Patel</h2>
+          <h3>Location:-Dhansura</h3>
+          <h3>Contact:-tejpatel1806@gmail.com</h3>
+        </div>
+      </>
+    );
+  };
+  export default User;
+  
